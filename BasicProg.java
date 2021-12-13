69be399d6f7c9f2e7410b77dda93beb314ca556b
sunilthitme <thitamesunil808@gmail.com>
2021-12-13 18:43:05 +0530

    Harmonic Number

--- BasicProg.java
+++ BasicProg.java
@@ -2,21 +2,20 @@ package com.basicprogrammes;
 import java.util.Scanner;
 public class BasicProg {
 
-		public static void main(String[] args) {
-			System.out.print("Enter number is (less than 31) ::");
-			Scanner sc=new Scanner(System.in);
-			int n=sc.nextInt();
-			if(n<=31) {
-			for( int i=1;i<=n;i++ )
-			{    
-				int table =2*i;
-			    System.out.println(table);
+	public static void main(String[] args) {
+		System.out.println("Welcome to harmonic number programme!!");
+		System.out.print("Please enter a Number::");
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		for(int i=1; i<=num; i++) {
+			
+			System.out.print( "1/"+  i );
+			if( i<num) {
+				System.out.print( " + " );
 			}
-			}
-			else {
-				System.out.println("Invalid number!!");
-			}
-
 		}
+		
 
 	}
+
+}
