821992097466623c85466cf0071ddb339a35d158
sunilthitme <thitamesunil808@gmail.com>
2021-12-13 20:00:20 +0530

    Factor

--- BasicProg.java
+++ BasicProg.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 public class BasicProg {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to harmonic number programme!!");
-		System.out.print("Please enter a Number::");
+		System.out.println("Welcome to a  factorization programme!!");
 		Scanner sc=new Scanner(System.in);
-		int num=sc.nextInt();
-		for(int i=1; i<=num; i++) {
+		 System.out.print("Enter a number:: ");
+		 int num=sc.nextInt();
+		boolean check=false;
+		 for(int i=2; i<=num/2; i++) {
+			 int rem=num%i;
 			
-			System.out.print( "1/"+  i );
-			if( i<num) {
-				System.out.print( " + " );
-			}
-		}
+			 if(rem==0) {
+				 System.out.println("The  Factor of "+ num+ " is "+i);
+               check=true;					 
+			 }
+		 }
 		
-
+           if(check==false) {
+        	  
+        	   System.out.println("The  Factor of "+ num+ " is "+num+" and "+1);
+           }
+        }
 	}
 
-}
